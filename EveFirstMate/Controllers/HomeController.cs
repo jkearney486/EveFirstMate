477f35c1c3a04d7068708fa8826bd5687a66f034
Joe Kearney <jkearney486@gmail.com>
2013-05-10 17:09:42 -0400

    added JsonNetResult
    Added a Base Controller that overrides the MVC Controller's Json()
    functions to use Json.NET instead of the built in serializer.

--- EveFirstMate/Controllers/HomeController.cs
+++ EveFirstMate/Controllers/HomeController.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EveFirstMate.Common;
 
 namespace EveFirstMate.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
         public ActionResult Index()
         {
