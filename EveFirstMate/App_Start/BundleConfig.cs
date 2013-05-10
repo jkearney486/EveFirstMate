bf23a1381c32140f3b642d78f4448d47f1991667
Joe Kearney <jkearney486@gmail.com>
2013-05-09 21:38:57 -0400

    added the project file

---
+++ EveFirstMate/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace EveFirstMate
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // Modernizr
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/libs/modernizr-{version}.js"));
+
+            // Global scripts
+            bundles.Add(new ScriptBundle("~/bundles/main").Include(
+                        "~/Scripts/jquery/jquery-{version}.js",
+                        "~/Scripts/jquery/jquery-ui-{version}.js",
+                        "~/Scripts/main.js"));
+
+            // Knockout & Knockout-mapping
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                                    "~/Scripts/knockout/knockout-{version}.debug.js",
+                                    "~/Scripts/knockout/knockout.mapping.debug.js"));
+
+            // Global styles
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                                    "~/Content/normalize.css",
+                                    "~/Content/themes/smoothness-mod/jquery-ui-1.10.2.css",
+                                    "~/Content/main.css"));
+        }
+    }
+}
---
+++ EveFirstMate/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EveFirstMate.MvcApplication" Language="C#" %>
