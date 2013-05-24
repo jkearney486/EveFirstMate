6a1ee23d6dd00ce0e935f3f99cb46ba06e0c27e3
Joe Kearney <jkearney486@gmail.com>
2013-05-24 13:04:52 -0400

    First pass at tree view implementation
    The market tree view uses data-node-state and data-leaf-state to open
    and close nodes on the tree. The leaf nodes are technically not true
    leaves in that they have children themselves, but they are the last
    nodes that 'open' and based on the behavior in the EVE Market, only one
    of these can be open at one time, so this has been implemented the same
    way.

--- EveFirstMate/Scripts/main.js
+++ EveFirstMate/Scripts/main.js
@@ -2,6 +2,37 @@
     "use strict";
 
     $(function () {
-        
+        $("#marketGroups").on("click", "li[data-node-state]", function (event) {
+            var currentState = this.dataset.nodeState,
+                nextState = currentState === "closed" ? "open" : "closed";
+
+            this.dataset.nodeState = nextState;
+            // don't propogate the event up previous nodes
+            return false;
+        });
+
+        $("#marketGroups").on("click", "li[data-leaf-state]", function (event) {
+            var currentState = this.dataset.leafState,
+                nextState = currentState === "closed" ? "open" : "closed";
+
+            // we want to limit the number of open leaf nodes to one
+            if (nextState === "open") {
+                // if any leaf nodes are open, close them
+                $("#marketGroups li[data-leaf-state=\"open\"]").each(function (index) {
+                    this.dataset.leafState = "closed";
+                });
+            }
+
+            this.dataset.leafState = nextState;
+            // don't propogate the event up previous nodes
+            return false;
+        });
+
+        $("#marketGroups").on("click", "li[data-item-typeid]", function (event) {
+            
+
+
+            return false;
+        });
     });
 }());
