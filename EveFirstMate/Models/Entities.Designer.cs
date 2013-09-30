1a632b08145568c320a62edec457479b67ae63fc
Joe Kearney <jkearney486@gmail.com>
2013-09-30 17:02:02 -0400

    BY FIRE BE PURGED!

--- EveFirstMate/Models/Entities.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\jkearney\Documents\GitHub\EveFirstMate\EveFirstMate\Models\Entities.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
